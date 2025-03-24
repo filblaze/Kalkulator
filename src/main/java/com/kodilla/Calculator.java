f7b9e9354eb448438eb338f491aaf2b02e8ab3d7
Filip Błażewicz <filipblazewicz@gmail.com>
2025-03-24 22:10:03 +0100

    Kalkulator1

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+
+    public double subtractBFromA(double a, double b) {
+        return a - b;
+    }
+}
